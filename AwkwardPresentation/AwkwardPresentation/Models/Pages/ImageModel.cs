cca6a058f90f91ca4c5efd744ac06253cc53d656
Vegard Solheim <vegard.solheim@inmeta.no>
2018-03-16 14:41:05 +0100

    Added ImageModel and PresentationModel

---
+++ AwkwardPresentation/AwkwardPresentation/Models/Pages/ImageModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AwkwardPresentation.Models.Pages
+{
+    [ContentType(DisplayName = "ImageModel", GUID = "44aba054-372e-4641-bd70-99a0f515b081", Description = "")]
+    public class ImageModel : PageData
+    {
+
+        [Display(
+            Name = "Image URL",
+            Description = "",
+            GroupName = SystemTabNames.Content,
+            Order = 100)]
+        public virtual string Url { get; set; }
+
+        [Display(
+            Name = "Image title",
+            Description = "",
+            GroupName = SystemTabNames.Content,
+            Order = 110)]
+        public virtual string Title { get; set; }
+
+        [Display(
+            Name = "Image text",
+            Description = "",
+            GroupName = SystemTabNames.Content,
+            Order = 120)]
+        public virtual string Text { get; set; }
+
+    }
+}
---
+++ AwkwardPresentation/AwkwardPresentation/Models/Pages/PresentationModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace AwkwardPresentation.Models.Pages
+{
+    [ContentType(DisplayName = "PresentationModel", GUID = "6bd7cc52-a0fb-4c48-82d9-4c3dfa0a50a4", Description = "")]
+    public class PresentationModel : PageData
+    {
+
+        [Display(
+            Name = "ID",
+            Description = "",
+            GroupName = SystemTabNames.Content,
+            Order = 100)]
+        public virtual int Id { get; set; }
+
+
+        [Display(
+            Name = "Main body",
+            Description = "",
+            GroupName = SystemTabNames.Content,
+            Order = 110)]
+        public virtual IList<ImageModel> Images { get; set; }
+    }
+}
